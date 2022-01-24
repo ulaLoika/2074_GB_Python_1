e214d47f62130a261b38c9cd8115e25ef0d36459
ulaLoika <75748247+ulaLoika@users.noreply.github.com>
2022-01-24 15:34:33 +0100

    Delete Uladzislava_Loika_lesson_2_task_1.1.py

--- Uladzislava_Loika_lesson_2_task_1.1.py
+++
@@ -1,8 +0,0 @@
-def identify_type (element):
-    return type(element)
-
-print (identify_type(15 * 3))
-print (identify_type(15 / 3))
-print (identify_type(15 // 2))
-print (identify_type(15 ** 2))
-
