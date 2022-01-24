1136d140c202fbf40ca26905464d77f47e1e1024
Ula Loika <uladzia.loyko@gmail.com>
2022-01-24 15:24:26 +0100

    задание сумма квадратов нечетных чисел

---
+++ Uladzislava_Loika_lesson_2_task_1.1.py
@@ -0,0 +1,8 @@
+def identify_type (element):
+    return type(element)
+
+print (identify_type(15 * 3))
+print (identify_type(15 / 3))
+print (identify_type(15 // 2))
+print (identify_type(15 ** 2))
+
